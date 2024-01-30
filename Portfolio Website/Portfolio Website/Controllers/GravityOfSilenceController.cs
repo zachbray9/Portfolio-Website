8edd4d32fbd76324d9a8f4fb052faf69f3c18be8
Zachary Bray <zachbray9@knights.ucf.edu>
2024-01-29 22:12:44 -0500

    Added Page for Gravity of Silence
    Added a section in the projects section for my UCF game production game, The Gravity of Silence. Also created a new page that contains more information about it. Also refactored my site.css file to get rid of unnecessary and duplicate styles.

---
+++ Portfolio Website/Portfolio Website/Controllers/GravityOfSilenceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Website.Controllers
+{
+    public class GravityOfSilenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
