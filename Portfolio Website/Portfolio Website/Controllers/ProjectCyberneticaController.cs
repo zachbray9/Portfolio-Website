590355e1cc3ed38d83834d7aad39d4e3f12898ab
Zachary Bray <zachbray9@knights.ucf.edu>
2024-01-30 23:39:40 -0500

    Began adding the Project Cybernetica page
    Added the project cybernetica page and began adding details. Still need to add the rest of the contributions section.

---
+++ Portfolio Website/Portfolio Website/Controllers/ProjectCyberneticaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Website.Controllers
+{
+    public class ProjectCyberneticaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
