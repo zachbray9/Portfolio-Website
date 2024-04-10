90c39c58eae327fac23133ca93f1fa2050ba2174
Zachary Bray <zachbray9@knights.ucf.edu>
2024-04-10 19:58:22 -0400

    Added AgilePro react app to project section and updated resume
    Added AgilePro React app to projects section, updated resume, and added skill tags for React and Chakra UI.

--- Portfolio Website/Portfolio Website/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++ Portfolio Website/Portfolio Website/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//fill navbar when user is not at the top of the page
+window.addEventListener('scroll', function ()
+{
+    var navbar = document.getElementById('navbar');
+
+    if (window.scrollY > 50)
+    {
+        navbar.classList.add('navbar-filled');
+    }
+    else
+    {
+        navbar.classList.remove('navbar-filled');
+    }
+});
+
